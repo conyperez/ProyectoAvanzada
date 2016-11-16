4365ee8857ba50a6defd3f7d35acad96f400835e
FelipeSegovia <felipe.segovia.rod@gmail.com>
2016-11-16 18:42:13 -0300

    Agrego la instancia leer archivo

--- ProyectoAvanzada/ProyectoAvanzada/Program.cs
+++ ProyectoAvanzada/ProyectoAvanzada/Program.cs
@@ -10,8 +10,8 @@ namespace ProyectoAvanzada
     {
         static void Main(string[] args)
         {
-            // Holi
-            //Funcionoooo
+            LeerArchivo arch = new LeerArchivo("Módulo2", "MóduloInferir");
+            arch.LeerPautas();
         }
     }
 }
