bc43bc53a4c76acdb6d61d2ede8571aa4c07bd8c
FelipeSegovia <felipe.segovia.rod@gmail.com>
2016-11-19 16:31:33 -0300

    Actualizacion PruebaDiagnostico
    Se agregan 3 constructores, se comentan los metodos y se cambia el nombre a la clase

--- ProyectoAvanzada/ProyectoAvanzada/Modelo/PruebaDiagnostico.cs
+++ ProyectoAvanzada/ProyectoAvanzada/Modelo/PruebaDiagnostico.cs
@@ -6,21 +6,45 @@ using System.Threading.Tasks;
 
 namespace ProyectoAvanzada.Modelo
 {
-    public class PruebaDiagnostico
+      public class Evaluaciones
     {
-        private List<string> actividad = new List<string>();
         private List<string> pauta = new List<string>();
         private List<string> respuestas = new List<string>();
+        private List<string> actividad = new List<string>();
+        private String Curso, NombreCarpeta, NombreModulo, TipoModulo;
+
+        public Evaluaciones(String Curso, String NombreCarpeta)
+        { //Para la prueba de diagnostico
+            this.Curso = Curso;
+            this.NombreCarpeta = NombreCarpeta;
+        }
+
+        public Evaluaciones(String Curso, String NombreCarpeta, String NombreModulo, String TipoModulo)
+        { //Para el modulo1.
+            this.Curso = Curso;
+            this.NombreCarpeta = NombreCarpeta;
+            this.NombreModulo = NombreModulo;
+            this.TipoModulo = TipoModulo;
+        }
+
+        public Evaluaciones(String Curso, String NombreCarpeta, String NombreModulo)
+        {
+            //Para los demas modulos
+            this.Curso = Curso;
+            this.NombreCarpeta = NombreCarpeta;
+            this.NombreModulo = NombreModulo;
+
+        }
 
         public void TrabajarActividad()
         {
-            LeerArchivo archivo = new LeerArchivo("Quinto Básico", "Evaluación Diagnóstico");
+            LeerArchivo archivo = new LeerArchivo(Curso, NombreCarpeta, NombreModulo, TipoModulo);
 
             archivo.setDireccion(@"\Actividades");
 
             int cantidad = archivo.CantidadArchivos();
             //Numero de actividad
-            actividad = archivo.LeerActividad(0);
+            actividad = archivo.LeerArchivos(0); //elige la actividad que se va a realizar
 
             for (int i = 0; i < actividad.Count; i++)
             {
@@ -32,21 +56,12 @@ namespace ProyectoAvanzada.Modelo
                 //Preguntas de seleccion
                 if (actividad.ElementAt(i).Equals("$"))
                 {
-
                     Console.WriteLine(actividad.ElementAt(i + 1));
-                    //Console.WriteLine(i);
-
-                    //codigos.Add(actividad.ElementAt(i));
-
                 }
                 if (actividad.ElementAt(i).Equals("$@"))
                 {
-                    //codigos.Add(actividad.ElementAt(i));
                     Console.WriteLine(actividad.ElementAt(i + 1));
-                    // Console.WriteLine(i);
-
                 }
-
                 if (actividad.ElementAt(i).Equals("@"))
                 {
                     Console.WriteLine(actividad.ElementAt(i + 1));
@@ -74,14 +89,14 @@ namespace ProyectoAvanzada.Modelo
 
         public void RevisarActividad()
         {
-            LeerArchivo acvitivdad = new LeerArchivo("Quinto Básico", "Evaluación Diagnóstico");
+            LeerArchivo acvitivdad = new LeerArchivo(Curso, NombreCarpeta, NombreModulo, TipoModulo);
 
             acvitivdad.setDireccion(@"\Pautas");
 
-            int cantidad = acvitivdad.CantidadArchivos();
+            int cantidad = acvitivdad.CantidadArchivos();//elige la pauta que es de la actividad
 
             //Numero de pauta
-            pauta = acvitivdad.LeerActividad(0);
+            pauta = acvitivdad.LeerArchivos(0);
 
             for (int i = 0; i < respuestas.Count; i++)
             {
