d0220839e00c8d4dd4be89e74ea6fe4d603437cd
FelipeSegovia <felipe.segovia.rod@gmail.com>
2016-11-23 00:54:39 -0300

    Se agrega la funcion TrabajoDiagnostico()

--- ProyectoAvanzada/ProyectoAvanzada/Modelo/Modelo.cs
+++ ProyectoAvanzada/ProyectoAvanzada/Modelo/Modelo.cs
@@ -10,10 +10,14 @@ namespace ProyectoAvanzada.Modelo
     {
         private String ResultadoH1=null, ResultadoH2=null; //Almaceno si es logrado o no logrado en cada habiliad.
         private Double Porcent_Act_Diag;
+        private Evaluaciones evaluacion;
+        private Diagnostico diagnostico;
+        private int CantActividad;
+        private int H1C = 0, H1I = 0, H2C = 0, H2I = 0;
         public Modelo() {
 
-            Evaluaciones evaluacion = new Evaluaciones();
-            Diagnostico diagnostico = new Diagnostico();
+            evaluacion = new Evaluaciones();
+            diagnostico = new Diagnostico();
             
 
             LeerArchivo archivos_actividad = new LeerArchivo("Quinto Básico", "Evaluación Diagnóstico");
@@ -26,9 +30,9 @@ namespace ProyectoAvanzada.Modelo
             diagnostico.setArchivo(archivos_pauta);
             diagnostico.setNombreCarpeta("Evaluación Diagnóstico");
 
-            List<string> respuestas = evaluacion.TrabajaActividad(0);
+            CantActividad = archivos_actividad.getCantArchivos();
 
-            diagnostico.RevisarActividad(respuestas,0);
+            Console.WriteLine(TrabajoDiagnostico());
 
 
             ResultadoH1 = diagnostico.getResultadoH1();
@@ -39,6 +43,39 @@ namespace ProyectoAvanzada.Modelo
             Console.WriteLine(Porcent_Act_Diag+"%");
 
             Console.ReadKey();
+        }
+
+        public String TrabajoDiagnostico() { //Esto va a la BD
+            string resultado=null; //Aqui devuelve si realiza el modulo 1.1 o 1.2
+            String ResultadoH1, ResultadoH2 = null;
+            List<String> respuestas;
+            for (int i=0;i< CantActividad;i++)//Bucle para realizar todas las actividades.
+            {
+                respuestas = evaluacion.TrabajaActividad(i); //contiene las respuestas que aplico el alumno
+                diagnostico.RevisarActividad(respuestas,i); //revisa esas respuestas con la pauta.
+                //Almacenar la cantidad de buenas y malas en cada pregunta.
+                H1C = H1C + diagnostico.getH1C();
+                H1I = H1I + diagnostico.getH1I();
+                H2C = H2C + diagnostico.getH2C();
+                H2I = H2I + diagnostico.getH2I();
+
+            }
+            //despues de realizar la revision de todas las actividades, determino el nivel de logro.
+            ResultadoH1 = diagnostico.determinarNivelLogroHabilidad(H1C,H1I);
+            ResultadoH2 = diagnostico.determinarNivelLogroHabilidad(H2C, H2I);
+            //ahora determinar cual habilidad enfocar
+            if (ResultadoH1.Equals(ResultadoH2)) { //Si ambos son iguales
+                return resultado = "MóduloEF";
+            }
+            if (ResultadoH1.Equals("No Logrado")) {
+                return resultado = "MóduloE";
+            }
+            if (ResultadoH2.Equals("No Logrado")) {
+                return resultado = "MóduloF";
+            }
+
+            return resultado;
+            
         }
 
     }
