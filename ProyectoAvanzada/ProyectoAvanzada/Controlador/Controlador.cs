29b1402684be2ddaa9838b026e0d8d7a0a68d41d
Constanza Pérez Merry <conny.caamila@gmail.com>
2016-11-26 21:18:49 -0300

    Update Controlador.cs

--- ProyectoAvanzada/ProyectoAvanzada/Controlador/Controlador.cs
+++ ProyectoAvanzada/ProyectoAvanzada/Controlador/Controlador.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,27 @@ namespace ProyectoAvanzada.Controlador
 {
     class Controlador
     {
+        Modelo.ConexionBD conexion;
+        Modelo.Modelo modelo;
+        string rut;
+
+        public Controlador(string rut)
+        {
+            this.rut = rut;
+            conexion = new Modelo.ConexionBD();
+            modelo = new Modelo.Modelo();
+        }
+
+        public void realizarDiagnostico()
+        {
+            Boolean realizado = conexion.diagnosticoRealizado(rut);
+            if(realizado == true)
+            {
+                modelo.TrabajoDiagnostico(rut);
+            } else
+            {
+                modelo.TrabajoDiagnostico(rut);
+            }
+        }
     }
 }
